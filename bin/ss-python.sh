de4e1219c8c31177ff71012ff4f9f9d5de0590b5
zengshaojian <zengshaojian@star-net.cn>
2017-12-25 19:00:41 +0800

    update start sh

--- bin/ss-python.sh
+++ bin/ss-python.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+bin=$(dirname $0)
+
 if [ `whoami` = 'root' ];then
     echo 'start ss-python now'
-    python3 ../ss-python/main.py &
+    python3 $bin/../ss-python/main.py &
 else
     echo 'need root to work'
 fi
